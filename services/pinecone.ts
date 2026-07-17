9a3e85d9ea3da032d8b0fa05f1d356ee42aaa4e6
Aryaveer <aryaveer1214@gmail.com>
2026-07-17 18:14:10 +0530

    add docs for pinecone service.

---
+++ services/pinecone.ts
@@ -0,0 +1,3 @@
+// https://docs.pinecone.io/guides/index-data/create-an-index#bring-your-own-vectors
+// https://docs.pinecone.io/guides/index-data/upsert-data#upsert-dense-vectors
+// https://docs.pinecone.io/guides/search/semantic-search#search-with-a-dense-vector
